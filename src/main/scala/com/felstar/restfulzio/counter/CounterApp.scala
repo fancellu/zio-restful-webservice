b4a63fb55b29ce1eaa431390b49d2f6b964f1f12
Dino Fancellu <dino@felstar.com>
2022-08-15 12:12:23 +0100

    CounterApp: Added some logging, changed "up" to use a for comprehension as way of example

--- src/main/scala/com/felstar/restfulzio/counter/CounterApp.scala
+++ src/main/scala/com/felstar/restfulzio/counter/CounterApp.scala
@@ -2,28 +2,36 @@ package com.felstar.restfulzio.counter
 
 import zhttp.http._
 import zio.{Ref, ZIO}
+import zio.Console._
 
-/**
- * An http app that:  
- *  - Accepts `Request` and returns a `Response`
- *  - Does not fail
- *  - Requires the `Ref[Int]` as the environment
- */
+/** An http app that:
+  *   - Accepts `Request` and returns a `Response`
+  *   - Does not fail
+  *   - Requires the `Ref[Int]` as the environment
+  */
 object CounterApp {
   def apply(): Http[Ref[Int], Nothing, Request, Response] =
     Http.fromZIO(ZIO.service[Ref[Int]]).flatMap { ref =>
       Http.collectZIO[Request] {
         case Method.GET -> !! / "up" =>
-          ref.updateAndGet(_ + 1)
-            .map(_.toString).map(Response.text)
+          for {
+            i <- ref.updateAndGet(_ + 1)
+            _ <- printLine(s"Incremented to $i").orDie
+          } yield Response.text(i.toString)
         case Method.GET -> !! / "down" =>
-          ref.updateAndGet(_ - 1)
-            .map(_.toString).map(Response.text)
+          ref
+            .updateAndGet(_ - 1)
+            .tap { i => printLine(s"Decremented to $i").orDie }
+            .map(_.toString)
+            .map(Response.text)
         case Method.GET -> !! / "get" =>
           ref.get.map(_.toString).map(Response.text)
         case Method.GET -> !! / "reset" =>
-          ref.updateAndGet(_=>0)
-            .map(_.toString).map(Response.text)
+          ref
+            .updateAndGet(_ => 0)
+            .tap { i => printLine(s"Reset to $i").orDie }
+            .map(_.toString)
+            .map(Response.text)
       }
     }
 }
