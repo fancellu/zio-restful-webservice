ef06a76618a6760b9ce792bf8aff87fa9d5a806b
Dino Fancellu <dino@felstar.com>
2022-08-18 16:52:45 +0100

    Added VideoAppSpec

---
+++ src/test/scala/com/felstar/restfulzio/videos/VideoAppSpec.scala
@@ -0,0 +1,22 @@
+package com.felstar.restfulzio.videos
+
+import zhttp.http.Method.POST
+import zhttp.http._
+import zio.json.DecoderOps
+import zio.test._
+
+object VideoAppSpec extends ZIOSpecDefault {
+
+  val app = VideoApp()
+
+  def spec = suite("VideoAppSpec")(
+    test("Loaded films are present") {
+      for {
+        _ <- app(Request(method=POST, url = URL(!! / "videos"/ "film1")))
+        _ <- app(Request(url = URL(!! / "videos" / "loadup")))
+        expectedBody <- app(Request(url = URL(!! / "videos"))).flatMap(_.bodyAsString)
+        videos = expectedBody.fromJson[List[Video]].toOption
+      } yield assertTrue(videos.map(_.map(_.name).toSet==Set("one","two","three", "four","film1")).getOrElse(false))
+    }
+  ).provide(InmemoryVideoRepo.layer)
+}
